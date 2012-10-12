7221d9b2c1113e4739a01cad1ed9e9f6e7720469
Matt Wright <matt@nobien.net>
2012-10-12 12:46:12 -0400

    Bump version number to 1.5.1

--- flask_social/__init__.py
+++ flask_social/__init__.py
@@ -10,7 +10,7 @@
     :license: MIT, see LICENSE for more details.
 """
 
-__version__ = '1.5.1-dev'
+__version__ = '1.5.1'
 
 from .core import Social
 from .datastore import SQLAlchemyConnectionDatastore, \
