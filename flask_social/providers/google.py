c87305714360fea662803737bc49db1893983caf
Paul Bonser <pib@paulbonser.com>
2014-02-17 17:25:52 -0600

    Concat name into string from google

--- flask_social/providers/google.py
+++ flask_social/providers/google.py
@@ -31,9 +31,11 @@ config = {
     'request_token_params': {
         'response_type': 'code',
         'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/plus.me'
-        #add ' https://www.googleapis.com/auth/userinfo.email' to scope to also get email
+        # add ' https://www.googleapis.com/auth/userinfo.email' to scope to
+        # also get email
     }
 }
+
 
 def _get_api(credentials):
     http = httplib2.Http()
@@ -73,13 +75,15 @@ def get_connection_values(response, **kwargs):
     )
 
     profile = _get_api(credentials).userinfo().get().execute()
+    full_name = ' '.join(
+        (profile['name']['givenName'], profile['name']['familyName']))
     return dict(
         provider_id=config['id'],
         provider_user_id=profile['id'],
         access_token=access_token,
         secret=None,
-        display_name=profile['name'],
-        full_name=profile['name'],
+        display_name=profile['displayName'],
+        full_name=full_name,
         profile_url=profile.get('link'),
         image_url=profile.get('picture')
     )
